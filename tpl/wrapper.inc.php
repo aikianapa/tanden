a49dc4c168755a33e3120bd5bfec7c2813da71fe
Oleg Frolov <oleg_frolov@mail.ru>
2020-08-24 07:29:13 +0300

    update

--- tpl/wrapper.inc.php
+++ tpl/wrapper.inc.php
@@ -75,7 +75,7 @@
 
 
 		<!-- Yandex.Metrika counter -->
-		<script type="wbapp" >
+		<script type="wbapp" wb-if='"{{_route.hostname}}" != "work9.loc"'>
 		   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 		   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
 		   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -87,7 +87,7 @@
 		        trackHash:true
 		   });
 		</script>
-		<noscript><div><img src="https://mc.yandex.ru/watch/20188255" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+		<noscript wb-if='"{{_route.hostname}}" != "work9.loc"'><div><img src="https://mc.yandex.ru/watch/20188255" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 		<!-- /Yandex.Metrika counter -->
 
 		<script type="wbapp">
