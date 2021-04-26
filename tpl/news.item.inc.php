dde2cae274956961bdbdc03a3998f6ada644f4c0
root <root@digiport.ru>
2021-04-26 10:16:48 +0000

    modified:   tpl/news.item.inc.php 	modified:   tpl/style.css

--- tpl/news.item.inc.php
+++ tpl/news.item.inc.php
@@ -1,8 +1,9 @@
 <div class="col-md-4 blog-item">
 		<wb-var link="/news/{{id}}/{{wbFurlGenerate({{header}})}}" wb-if='"{{type}}" == "news"' />
 		<wb-var link="/articles/{{id}}/{{wbFurlGenerate({{header}})}}" wb-if='"{{type}}" == "article"' />
-		<a class="image-link" href="{{_var.link}}">
-				<img data-src="/thumbc/360x240/src/{{images.0.img}}" alt="{{header}}" class="img-responsive">
+		<a class="image-link" href="{{_var.link}}" 
+		style="background-image: url(/thumbc/50x50/src/{{images.0.img}})">
+				<img data-src="/thumb/360x240/src/{{images.0.img}}" alt="{{header}}" class="img-responsive">
 		</a>
 		<h2>{{header}}</h2>
 		<p>{{wbGetWords({{text}},20)}}</p>
